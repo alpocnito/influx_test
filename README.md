9274b4b52646ae6b25cba549cbb4fba34c2efb85
Руслан Зарипов <zaripov.ra@phystech.edu>
2024-04-20 14:38:03 +0300

    clear repo

--- README.md
+++ README.md
@@ -9,7 +9,12 @@ docker compose up -d
 poetry install
 ```
 
-В файле ***test_audio.ipynb*** представлен пример работы
-1. Записываем аудио из файла raw/horse.mp3 в базу данных в bucket='bucket_in'
-2. В базе данных находиться задача (task), которая увеличивает громкость данных из bucket='bucket_in' и кладет их в bucket='bucket_out'
-3. Далее мы читаем данные из bucket='bucket_out' и преобразуем обратно в аудио raw/horse_out.mp3
+Запускаем в первой консоли считывание аудио из базы данных
+```
+poetry run python3 read.py
+```
+
+Во второй консоли запускаем запись аудио в базу данных
+```
+poetry run python3 write.py
+```
---
+++ write.py
@@ -0,0 +1,45 @@
+import time
+import sounddevice as sd
+import influxdb_client
+
+duration_sec = 2000
+
+# Запись в influxDB
+org = "mipt"
+token='AxZQBiPLqGo7nkwQxuga16V_Ui8qY8livmN-TGKiqtTO3LOfAd3KNRslGYs4Eorcml1IgYlnwb8YtzM1jX22-Q=='
+url="http://localhost:8086"
+bucket = "bucket1"
+
+client = influxdb_client.InfluxDBClient(
+    url=url,
+    token=token,
+    org=org
+)
+
+global_time = 0
+def callback(indata, outdata, frames, _time, status):
+    global global_time
+
+    print('glob:', time.time() - global_time)
+    points = []
+    start = time.time()
+    for val in indata:
+        write_time = int(time.time() * 1_000_000_000) 
+        point = {
+            "measurement": "a",
+            "time": write_time,
+            "tags": {
+                "a": "i",
+            },
+            "fields": {
+                "l": float(val[0]),
+            }
+        }
+        points.append(point)
+    with client.write_api() as write_api:
+        write_api.write(bucket=bucket, org=org, record=points)
+    print('proc:', time.time() - start)
+    global_time = time.time()
+
+with sd.Stream(channels=1, callback=callback, blocksize=44100):
+    sd.sleep(int(duration_sec * 1000))
